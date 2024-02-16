4b5cc45fd6b1738548280b267389913598bfdf54
Luis Sandoval <LSandoval@badgermeter.com>
2024-02-16 12:28:52 -0700

    comentarios

--- public/index.js
+++ public/index.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
   validarConexion();
 });
 
+// Validamos que el raspberry pi este conectado
 const validarConexion = async () => {
   
   try {
@@ -35,6 +36,8 @@ const validarConexion = async () => {
     const { minutos } = result
     const isDisconnected = minutos > IS_DISCONNECTED
     
+    // Si no detecta un cambio en el ultimo registro muestra aviso
+    // Se ejecuta una sola vez al cargar la pagina
     TEMPERATURA.classList.toggle('text-danger', isDisconnected)
     if(isDisconnected) {
       TEMPERATURA.textContent = SIN_CONEXION
@@ -51,12 +54,14 @@ const validarConexion = async () => {
 
 const obtenerTemperatura = async () => {
 
+  // Ejecutamos la consulta cada 15 segundos
   setInterval(() => {
 
     (async () => {
       try {
         const TEMPERATURA_LIMITE_ACTUAL = Number(TEMPERATURA_LIMITE.value).toFixed(2)
 
+        // Realizamos la peticion para obtener la ultima temperatura registrada
         const response = await fetch('https://us-east-1.aws.data.mongodb-api.com/app/application-0-lgljv/endpoint/obtenerUltimaTemperatura');
 
         if (!response.ok) throw new Error('Network response was not ok');
@@ -69,10 +74,12 @@ const obtenerTemperatura = async () => {
         const toNumber = Number(temperatura).toFixed(2)
         TEMPERATURA.textContent = toNumber
         
+        // Validamos si la temperatura supero el 10% de la temperatura limite establecida
         if(toNumber >= (TEMPERATURA_LIMITE_ACTUAL * 1.10)) {
           AVISO.textContent = LIMITE_10
           const inicio = new Date(date)
 
+          // Si no ha sido guardada, se almacena para guardar la referencia
           if(localStorage.getItem('inicio_10') !== undefined) {
             localStorage.setItem('inicio_10', inicio)
           }
@@ -80,6 +87,8 @@ const obtenerTemperatura = async () => {
         } else {
           AVISO.textContent = ''
 
+          // Calculamos la diferencia de tiempo entre la fecha guardada y la fecha en la que la temperatura bajo del limite del 10%
+          // Borramos la temperatura guardada y mostramos el tiempo en minutos y segundos
           if(localStorage.getItem('inicio_10')) {
             const inicio = localStorage.getItem('inicio_10')
             const result = calcularDiferenciaTiempo(new Date(inicio), new Date())
@@ -90,10 +99,12 @@ const obtenerTemperatura = async () => {
           }
         }
 
+        // Validamos si la temperatura supero el 20% de la temperatura limite establecida
         if(toNumber >= TEMPERATURA_LIMITE_ACTUAL * 1.20) {
           AVISO.textContent = LIMITE_20
           const inicio = new Date(date)
 
+          // Si no ha sido guardada, se almacena para guardar la referencia
           if(localStorage.getItem('inicio_20') !== undefined) {
             localStorage.setItem('inicio_20', inicio)
           }
@@ -101,6 +112,8 @@ const obtenerTemperatura = async () => {
         } else {
           AVISO.textContent = ''
 
+          // Calculamos la diferencia de tiempo entre la fecha guardada y la fecha en la que la temperatura bajo del limite del 20%
+          // Borramos la temperatura guardada y mostramos el tiempo en minutos y segundos
           if(localStorage.getItem('inicio_20')) {
             const inicio = localStorage.getItem('inicio_20')
             const result = calcularDiferenciaTiempo(new Date(inicio), new Date())
@@ -116,7 +129,7 @@ const obtenerTemperatura = async () => {
       }
     })();
 
-  }, 30000)
+  }, 10000)
 
 } 
 
